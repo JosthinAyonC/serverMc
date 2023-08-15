0b197ebe761ca6c833eaf7d4ae5145c54ba4e01e
JosthinAyon <josthinayon@gmail.com>
2023-08-14 22:20:14 -0500

    14/8/2023J

--- start.bat
+++
@@ -1,5 +0,0 @@
-TITLE Minecraft Server
-:loop
- java -Xmx4096M -Xms4096M -jar paper-1.20-17.jar nogui
-goto :loop
-pause
---
+++ startserver.bat
@@ -0,0 +1,3 @@
+@echo off
+java -Xms4G -Xmx4G -jar mohist-1.20.1-336-server.jar -nogui
+pause
